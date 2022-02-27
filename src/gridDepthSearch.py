5a02cc28a54de3de96dcf128791b26c32f8c5c3c
Simon Johanning <Simonjohanning@gmail.com>
2022-02-27 22:09:05 +0100

    code documentation and minor improvements

--- src/gridDepthSearch.py
+++ src/gridDepthSearch.py
@@ -3,26 +3,29 @@ import json
 import numpy as np
 import math
 
+# function to run the (repo-based current) version of the model instance with the conversation
+# given in the input file
 def invokeJar(inputFile, modeParameters):
-    # print(inputFile)
-    # print(modeParameters)
     try:
         data = check_output(
-            ['java', '-jar', 'src/ressources/IRPact-1.0-SNAPSHOT-uber.jar', '-i', inputFile + '.json', '-o', 'example-output.json',
+            ['java', '-jar', 'src/resources/IRPact-1.0-SNAPSHOT-uber.jar', '-i', inputFile + '.json', '-o', 'example-output.json',
              '--noConsole', '--logPath', 'log.log', '--calculatePerformance', modeParameters], shell=True)
         t = 0, data.decode('utf-8').rstrip()
         return data
     except CalledProcessError as e:
         t = e.returncode, e.message
 
+# mock function to test the rest of the module
 def mockInvokeJar(AT, IT):
     if(AT > 0):
         return (1/(AT*IT))
     else:
         return 999999
 
+# function that manipulates the scenario definition to fit the adoption and interest threshold for the desired run
+# file is saved in the path and prefix specified by the templateFile parameter
 def prepareJson(templateFile, adoptionThreshold, interestThreshold):
-    f = open('src/ressources/example-input.json', "r")
+    f = open('src/resources/example-input.json', "r")
     fileData = json.loads(f.read())
     fileData['data'][0]['years'][0]['sets']['set_InDiracUnivariateDistribution']['INTEREST_THRESHOLD'][
         'par_InDiracUnivariateDistribution_value'] = interestThreshold
@@ -31,31 +34,40 @@ def prepareJson(templateFile, adoptionThreshold, interestThreshold):
     with open(templateFile+"-"+str(adoptionThreshold)[2:len(str(adoptionThreshold))]+"-"+str(interestThreshold)+".json", "w") as file:
         json.dump(fileData, file)
 
-# calculate an even grid of the respective values for the thresholds in the specified execution
+# function to set up the simulation runs of the current iteration level
+# calculates the parameters for equidistant sampling in the respective parameter region
+# number of samples is given by the resolution (entry in searchParameters dictionary),
+# region is defined through the min and max of the AT and IT parameters
 def calculateGrid(searchParameters, minAT, maxAT, minIT, maxIT):
     list = [[0 for col in range(searchParameters['resolution'])] for row in range(searchParameters['resolution'])]
     performanceEvaluation = [0 for run in range(int(math.pow(searchParameters['resolution'], 2)))]
+    # prepare the grid for the simulation errors (2D list entries)
     for row in range(searchParameters['resolution']):
         for col in range(searchParameters['resolution']):
+            # parameters for entries are calculated equidistantly (currentX = respective run)
             currentAT = minAT + ((maxAT-minAT)/(searchParameters['resolution']-1))*row
             currentIT = round(minIT + ((maxIT-minIT)/(searchParameters['resolution']-1))*col)
-            print('calculating for IT '+str(currentIT)+' and AT '+str(currentAT))
+            if(searchParameters['printFlag']): print('calculating for IT '+str(currentIT)+' and AT '+str(currentAT))
             prepareJson('src/modelInputFiles/changedInterest', currentAT, currentIT)
+            # add the performance of the run to the list
             list[col][row] = float(invokeJar("src/modelInputFiles/changedInterest-" + str(currentAT)[2:len(str(currentAT))] + "-" + str(currentIT), searchParameters['errorDefinition']))
-            print('Calculating for index ' + str(row*searchParameters['resolution']+col) + ' with row ' + str(row) + ' and column ' + str(col))
+            if(searchParameters['printFlag']): print('Calculating for index ' + str(row*searchParameters['resolution']+col) + ' with row ' + str(row) + ' and column ' + str(col))
     #        list[col][row] = mockInvokeJar(currentAT, currentIT)
-            print('Performance of run ' + str(row*searchParameters['resolution']+col) + ': ' + str(list[col][row]))
+            if(searchParameters['printFlag']): print('Performance of run ' + str(row*searchParameters['resolution']+col) + ': ' + str(list[col][row]))
+            # evaluation of a single run is recorded in parameters and performance
             performanceEvaluation[row*searchParameters['resolution']+col] = {
                 "adoptionThreshold": currentAT,
                 "interestThreshold": currentIT,
-                "error": list[col][row]
+                "performance": list[col][row]
             }
     return list, performanceEvaluation
 
 # Evaluates a grid-based pattern in evenly spaced points (dim: interest threshold and adoption threshold)
 # and iterates the search in a sub-region containing the best performing points.
 # The size of the sub-region depends on the searchParameter scaleFactor (size of current region and next iteration per dimension)
-# and the number of points evaluated depends on the resolution
+# and the number of points evaluated depends on the resolution.
+# arguments are split in search parameters (that remain constant throughout the iteration)
+# and the search state that is individual to each run.
 # search parameters:
 #   acceptableDelta: what error is tolerable to terminate the search
 #   maxDepth: how many iterations can be made at most
@@ -66,17 +78,20 @@ def calculateGrid(searchParameters, minAT, maxAT, minIT, maxIT):
 # search state:
 #   currentDelta: deviation of the last run from the validated time series
 #   currentRecursionDepth: number of recursion steps made before
-#   evaluationData: performance and information of the previous runs
+#   evaluationData: performance and information of the previous runs; entries are lists for each sample in the iteration
 #   lowerBoundAT: the low end of the adoption threshold dimension to search in
 #   upperBoundAT: the high end of the adoption threshold dimension to search in
 #   lowerBoundIT: the low end of the interest threshold dimension to search in
 #   upperBoundIT: the high end of the interest threshold dimension to search in
 def nextDepthSearchIteration(searchParameters, searchState):
+    # if the last results are close enough to quality standards or 'too many' iterations have been performed, the search terminates
     if (searchState['currentDelta'] <= searchParameters['acceptableDelta'] or searchState['currentRecursionDepth'] >= searchParameters['maxDepth']):
         return searchState
     else:
-        print('In next search iteration. State is \n')
-        print(searchState)
+        if(searchParameters['printFlag']):
+            print('In next search iteration. State is \n')
+            print(searchState)
+        # results and information on the
         gridResults, resultDic = calculateGrid(searchParameters, searchState['lowerBoundAT'], searchState["upperBoundAT"], searchState["lowerBoundIT"], searchState["upperBoundIT"])
         gridResultList = np.array(gridResults)
         # Finding the index with the lowest error, adapted from https://devenum.com/find-min-value-index-in-numpy-array/
@@ -100,14 +115,20 @@ def nextDepthSearchIteration(searchParameters, searchState):
             return nextDepthSearchIteration(searchParameters, {
                 'currentDelta': gridResultList[currentMinIndex],
                 'currentRecursionDepth': searchState['currentRecursionDepth'] + 1,
-                "evaluationData": {},
+                "evaluationData": searchState['evaluationData'].append(resultDic),
                 "lowerBoundAT": newLowerBoundIT,
                 "upperBoundAT": newUpperBoundIT,
                 "lowerBoundIT": newLowerBoundAT,
                 "upperBoundIT": newUpperBoundAT
             })
 
+# the grid depth search iteratively samples smaller / finer regions of the parameter space
+# equidistantly until close enough to the reference time series or a given number of iterations are reached
+# initializes the first call with relatively broad parameters
+# returns the state of the iteration that terminates the search
 def iterateGridDepthSearch(acceptableDelta, maxDepth, scaleFactor, resolution, errorDefinition):
+    # clean the folder of former modelInputFiles
+    check_output(['rm', 'src/modelInputFiles/*.json', '-r', '-y'], shell=True)
     return nextDepthSearchIteration({
         'acceptableDelta': acceptableDelta,
         'maxDepth': maxDepth,
--- src/main.py
+++ src/main.py
@@ -1,14 +1,23 @@
 import gridDepthSearch
 import sys
 
+# module to initialize the optimization process and set the parameters.
+# Serves to isolate the functionality of the individual optimization methods from the data
 
+# function to manage the execution of the optimization by the chosen mode
 def runOptimization(acceptableDelta, errorDefinition, optimizationMethod):
     if (optimizationMethod == 'gridDepthSearch'):
-        gridDepthSearch.iterateGridDepthSearch(acceptableDelta, 5, 2, 3, errorDefinition)
+        # the grid depth search iteratively samples smaller / finer regions of the parameter space
+        # equidistantly until close enough to the reference time series or a given number of iterations are reached
+        # parameters are the acceptableDelta, maxDepth, scaleFactor, resolution and errorDefinition;
+        # see function for further documentation
+        print(gridDepthSearch.iterateGridDepthSearch(acceptableDelta, 5, 2, 3, errorDefinition))
 
+# main method to set up the logging and invoke the optimization management method function
 if __name__ == '__main__':
     log_file = open("message.log", "w")
     sys.stdout = log_file
+    # invokation of the search
     runOptimization(0.15, 'RMSD', 'gridDepthSearch')
     log_file.close()
 
